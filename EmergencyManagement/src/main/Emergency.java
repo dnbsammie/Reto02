ddd4266182fad6ae42e79afb58e8268214919c3b
Samuel Rivera <seriveramosq@gmail.com>
2025-06-04 20:13:20 -0500

    chore: new psvm

--- EmergencyManagement/src/main/Emergency.java
+++ EmergencyManagement/src/main/Emergency.java
@@ -3,8 +3,5 @@ package main;
 import java.util.Scanner;
 
 public class Emergency {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        input.close();
-    }
+
 }
---
+++ EmergencyManagement/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        input.close();
+    }
+}
