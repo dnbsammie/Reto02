71650a5419d562148ec0ca78d60e4107cf5bc787
Samuel Rivera <seriveramosq@gmail.com>
2025-06-06 20:12:08 -0500

    chore: change main method

--- EmergencyManagement/src/main/Main.java
+++ EmergencyManagement/src/main/Main.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        Emergency emergencySystem = new Emergency();
+        emergencySystem.start(input);
         input.close();
+        System.out.println("Programa finalizado correctamente.");
     }
 }
