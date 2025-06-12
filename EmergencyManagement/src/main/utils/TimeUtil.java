115a5ffbb03b46a5282cd7a6ccc8a0e7b707a7ec
Samuel Rivera <seriveramosq@gmail.com>
2025-06-11 20:19:57 -0500

    chore: add TimeUtil

--- EmergencyManagement/src/main/utils/TimeUtil.java
+++ EmergencyManagement/src/main/utils/TimeUtil.java
@@ -4,5 +4,12 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class TimeUtil {
+    public static void printTime(String message) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy");
 
+        System.out.printf("\n%s\n", message);
+        System.out.println("Hora actual:");
+        System.out.println(now.format(format) + "\n");
+    }
 }
